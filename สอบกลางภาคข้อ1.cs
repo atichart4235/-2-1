d5323c718e48ad9c6e9d579305fce6f993b94f8a
atichart4235 <chayanon.bunprasert@mail.kmutt.ac.th>
2021-10-03 01:24:29 +0700

    no message

---
+++ สอบกลางภาคข้อ1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+enum Menu
+{
+    Playgame = 1,
+    Exit = 2
+}
+namespace HangmanMDT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menuscreen();
+            InputMenu();
+        }
+        static void Menuscreen()
+        {
+            Console.WriteLine("Welcome to Hangman Game");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("1.Playgame");
+            Console.WriteLine("2.Exit");
+            
+            //หน้าแรก
+        }
+        static void InputMenu()
+        {
+            Console.Write("Select Menu:"); 
+            Menu menu = (Menu)int.Parse(Console.ReadLine());
+
+            PresentMenu(menu);
+
+            //ตัวเลือกเมนู
+        }
+        static void PresentMenu(Menu menu)
+        {
+            if (menu == Menu.Playgame)
+            {
+
+            }
+            else
+            {
+                Console.ReadLine();
+            }
+
+            Console.Clear();
+            Random random = new Random((int)DateTime.Now.Ticks);
+            
+            //การสุ่ม
+
+            string[] wordBank = { "Tennis", "Football", "Badminton" };
+
+            string wordToGuess = wordBank[random.Next(0, wordBank.Length)];
+            string wordToGuessUppercase = wordToGuess.ToUpper();
+
+            StringBuilder displayToPlayer = new StringBuilder(wordToGuess.Length);
+            for (int i = 0; i < wordToGuess.Length; i++)
+                displayToPlayer.Append('_');
+
+            List<char> correctGuesses = new List<char>();
+            List<char> incorrectGuesses = new List<char>();
+
+            int lives = 1;
+            bool win = false;
+            int lettersRevealed = 0;
+
+            string input;
+            char guess;
+
+            while (!win && lives != 7)
+            {
+                Console.WriteLine("Play Game Hangman");
+                Console.WriteLine("-----------------");
+                Console.Write("Input letter alphabet:");
+                
+                //Console แสดงผลหน้าเล่นเกม
+
+                input = Console.ReadLine().ToUpper();
+                guess = input[0];
+
+                if (correctGuesses.Contains(guess))
+                {
+                    Console.WriteLine("You've already tried '{0}', and it was correct!", guess);
+                    continue;
+                }
+                else if (incorrectGuesses.Contains(guess))
+                {
+                    Console.WriteLine("You've already tried '{0}', and it was wrong!", guess);
+                    continue;
+                }
+
+                if (wordToGuessUppercase.Contains(guess))
+                {
+                    correctGuesses.Add(guess);
+
+                    for (int i = 0; i < wordToGuess.Length; i++)
+                    {
+                        if (wordToGuessUppercase[i] == guess)
+                        {
+                            displayToPlayer[i] = wordToGuess[i];
+                            lettersRevealed++;
+                        }
+                    }
+
+                    if (lettersRevealed == wordToGuess.Length)
+                        win = true;
+                }
+                else
+                {
+                    incorrectGuesses.Add(guess);
+                    Console.Clear(); //เคลียร์เมื่อตอบผิด
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Incorrect Score: {0}", lives);
+                    Console.WriteLine("Nope, there's no '{0}' in it!", guess);
+                    lives++;
+                }
+                Console.WriteLine(displayToPlayer.ToString());
+            }
+            if (win)
+                Console.WriteLine("You win!");  //ถ้าตอบถูก
+            else
+           Console.WriteLine("Game Over IT was:'{0}'", wordToGuess); 
+            //ถ้าตอบผิดเกิน6ครั้งจะเกมโอเวอร์และบอกคำตอบ
+
+           Console.Write("Press ENTER to exit..."); //จบเกม
+           Console.ReadLine();
+        }
+    }
+}
---
+++ สอบกลางภาคข้อ2.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+enum Menu
+{
+    Login = 1, 
+    Register = 2, //หน้าเมนู
+    MeNuDisPlay = 3,
+}
+namespace Controllers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menuscreen();
+            InputMenu();
+        }
+        static void register()
+        {
+            Register reGister = new Register(); //หน้าสมัคร
+            Console.Write("Input Name:");  //ให้ใส่ชื่อ
+            reGister.Name = Console.ReadLine();
+            Console.Write("Input Password:"); //ให้ใส่พาส
+            reGister.Password = Console.ReadLine();
+            Console.Write("1=Student,2=Employee:"); //เลือกสถานะ
+            reGister.Type = Console.ReadLine();
+            if (reGister.Type == "1") //ถ้าเลือก1
+            {
+                Console.WriteLine("Register new Person");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Input name kittinum");
+                Console.WriteLine("Input Password 1234");
+                Console.WriteLine("Input User Type 1=Student,2=Employee:1");
+                Console.WriteLine("Student ID 62130701103");
+            }
+            else if (reGister.Type == "2") //ถ้าเลือก2
+            {
+                Console.WriteLine("Register new Person");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Input name kittinum");
+                Console.WriteLine("Input Password 1234");
+                Console.WriteLine("Input User Type 1=Student,2=Employee:2");
+                Console.WriteLine("Employee ID 5214785");
+            }
+        }
+        static void login() //หน้าล็อกอิน
+        {
+            Login logIn = new Login();
+            Console.Write("Input Name:"); //ใส่ชื่อ
+            logIn.Name = Console.ReadLine();
+            Console.Write("Input Password:"); //ใส่พาส
+            logIn.Password = Console.ReadLine();
+        }
+        static void meNuDisPlay()
+        {
+            MeNuDisPlay meNuDisPlay = new MeNuDisPlay();
+            Register reGister = new Register(); //หน้าสมัคร
+            Console.Write("Input Name:");  //ให้ใส่ชื่อ
+            reGister.Name = Console.ReadLine();
+            Console.Write("Input Password:"); //ให้ใส่พาส
+            Console.Write("1=Student,2=Employee:"); //เลือกสถานะ
+            meNuDisPlay.borrowbooks = Console.ReadLine();
+            if (meNuDisPlay.borrowbooks == "1") //ถ้าเลือก1
+            {
+                Console.WriteLine("Book List");
+                Console.WriteLine("---------");
+                Console.WriteLine("Book ID: 1");
+                Console.WriteLine("Book name: NOW I UNDERSTAND");
+                Console.WriteLine("Book ID: 2");
+                Console.WriteLine("Book name: REVOLUTIONARY WEALTH");
+                Console.WriteLine("Book ID: 3");
+                Console.WriteLine("Book name: Six Degrees");
+                Console.WriteLine("Book ID: 4");
+                Console.WriteLine("Book name: Les Vacances");
+                Console.WriteLine("Input book ID:");
+
+            }
+            else if (meNuDisPlay.bookstock == "2") //ถ้าเลือก2
+            {
+                Console.WriteLine("Book List");
+                Console.WriteLine("---------");
+                Console.WriteLine("Book ID: 1");
+                Console.WriteLine("Book name: NOW I UNDERSTAND");
+                Console.WriteLine("Book ID: 2");
+                Console.WriteLine("Book name: REVOLUTIONARY WEALTH");
+                Console.WriteLine("Book ID: 3");
+                Console.WriteLine("Book name: Six Degrees");
+                Console.WriteLine("Book ID: 4");
+                Console.WriteLine("Book name: Les Vacances");
+            }
+        }
+        static void Menuscreen()
+        {
+            Console.WriteLine("Welcome to Digital Library"); //หน้าแรก
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("1.Login"); //แสดงให้เลือกล็อคอิน
+            Console.WriteLine("2.Register"); //แสดงให้เลือกหน้าสมัคร
+        }
+        static void InputMenu()
+        {
+            Console.Write("Select Menu: "); //ให้เลือกหน้าเมนูระหว่างล็อคอินกับสมัคร
+            Menu menu = (Menu)(int.Parse(Console.ReadLine()));
+            Console.Clear();
+            PresentMenu(menu);
+        }
+        static void PresentMenu(Menu menu)
+        {
+            if (menu == Menu.Login)
+            {
+                login();
+            }
+            else if (menu == Menu.Register)
+            {
+                register();
+            }
+            else if (menu == Menu.MeNuDisPlay)
+            {
+                register();
+            }
+
+
+        }
+
+
+        public class Register //คลาสสมัคร
+        {
+            public string Name;
+            public string Password;
+            public string Type;
+
+        }
+        public class Login //คลาสล็อคอิน
+        {
+            public string Name;
+            public string Password;
+        }
+        public class MeNuDisPlay //คลาสแสดงเมนู
+        {
+            public string borrowbooks;
+            public string bookstock;
+        }
+    }
+}
+
+   
+
+    
+
+    
+
